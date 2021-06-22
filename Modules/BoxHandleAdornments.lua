62186b1a70ae890db2c7ed60bc5dcc88efb59599
iHavoc101 <69701784+iHavoc101@users.noreply.github.com>
2021-06-21 20:35:38 -0400

    Create BoxHandleAdornments.lua

---
+++ Modules/BoxHandleAdornments.lua
@@ -0,0 +1,134 @@
+-- // Constants \\ --
+local RunService = game:GetService("RunService")
+
+-- [ Modules ] --
+local MaidModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Maid.lua"))()
+
+-- [ LocalPlayer ] --
+local Camera = workspace.CurrentCamera
+
+-- // Variables \\ --
+local MakeCFrame, MakeVector3, WorldToViewportPoint, ClampNumber = CFrame.new, Vector3.new, Camera.WorldToViewportPoint, math.clamp
+local MakeDrawing = Drawing.new
+
+-- // Functions \\ --
+local function FetchViewportPoint(Position)
+    local ViewportPoint, OnScreen = WorldToViewportPoint(Camera, Position)
+    return Vector2.new(ViewportPoint.X, ViewportPoint.Y)
+end
+
+local function FetchSurfaces(CoordinateFrame, Size)
+    local Surfaces = {
+        (CoordinateFrame * MakeCFrame(-Size.X, Size.Y, -Size.Z)).Position,
+        (CoordinateFrame * MakeCFrame(-Size.X, Size.Y, Size.Z)).Position,
+        (CoordinateFrame * MakeCFrame(Size.X, Size.Y, Size.Z)).Position,
+        (CoordinateFrame * MakeCFrame(Size.X, Size.Y, -Size.Z)).Position,
+        (CoordinateFrame * MakeCFrame(-Size.X, -Size.Y, -Size.Z)).Position,
+        (CoordinateFrame * MakeCFrame(-Size.X, -Size.Y, Size.Z)).Position,
+        (CoordinateFrame * MakeCFrame(Size.X, -Size.Y, Size.Z)).Position,
+        (CoordinateFrame * MakeCFrame(Size.X, -Size.Y, -Size.Z)).Position
+    }
+
+    return {
+        UpperSurface = {Surfaces[1], Surfaces[2], Surfaces[3], Surfaces[4]},
+        LowerSurface = {Surfaces[5], Surfaces[6], Surfaces[7], Surfaces[8]},
+        SideSurface1 = {Surfaces[1], Surfaces[2], Surfaces[6], Surfaces[5]},
+        SideSurface2 = {Surfaces[3], Surfaces[4], Surfaces[8], Surfaces[7]},
+        SideSurface3 = {Surfaces[2], Surfaces[3], Surfaces[7], Surfaces[6]},
+        SideSurface4 = {Surfaces[1], Surfaces[4], Surfaces[8], Surfaces[5]}
+    }
+end
+
+local function FetchViewportPoints(CoordinateFrame, Size)
+    Size /= 2
+    local Surfaces = {
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(-Size.X, Size.Y, -Size.Z)).Position),
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(-Size.X, Size.Y, Size.Z)).Position),
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(Size.X, Size.Y, Size.Z)).Position),
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(Size.X, Size.Y, -Size.Z)).Position),
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(-Size.X, -Size.Y, -Size.Z)).Position),
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(-Size.X, -Size.Y, Size.Z)).Position),
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(Size.X, -Size.Y, Size.Z)).Position),
+        FetchViewportPoint((CoordinateFrame * MakeCFrame(Size.X, -Size.Y, -Size.Z)).Position)
+    }
+
+    return {
+        UpperSurface = {Surfaces[1], Surfaces[2], Surfaces[3], Surfaces[4]},
+        LowerSurface = {Surfaces[5], Surfaces[6], Surfaces[7], Surfaces[8]},
+        SideSurface1 = {Surfaces[1], Surfaces[2], Surfaces[6], Surfaces[5]},
+        SideSurface2 = {Surfaces[3], Surfaces[4], Surfaces[8], Surfaces[7]},
+        SideSurface3 = {Surfaces[2], Surfaces[3], Surfaces[7], Surfaces[6]},
+        SideSurface4 = {Surfaces[1], Surfaces[4], Surfaces[8], Surfaces[5]}
+    }
+end
+
+-- // Main Module \\ --
+local MainModule = {}
+MainModule.ClassName = "BoxHandleAdornment"
+MainModule.__index = MainModule
+
+function MainModule.new()
+    local BoxHandleAdornment = {
+        Name            = "BoxHandleAdornment",
+        Visible         = true,
+        Filled          = false,
+        Outlined        = true,
+        Color3          = Color3.new(1, 1, 1),
+        Transparency    = 0,
+        CFrame          = MakeCFrame(),
+        Size            = MakeVector3(),
+        _Maid           = MaidModule.new(),
+        _Drawings       = {
+            UpperSurface = MakeDrawing("Quad");
+            LowerSurface = MakeDrawing("Quad");
+            SideSurface1 = MakeDrawing("Quad");
+            SideSurface2 = MakeDrawing("Quad");
+            SideSurface3 = MakeDrawing("Quad");
+            SideSurface4 = MakeDrawing("Quad");
+        }
+    }
+
+    BoxHandleAdornment._Maid:GiveTask(RunService.RenderStepped:Connect(function()
+        local ViewportPoints = FetchViewportPoints(BoxHandleAdornment.CFrame, BoxHandleAdornment.Size)
+        local Distance = (Camera.CFrame.Position - BoxHandleAdornment.CFrame.Position).Magnitude
+        for i,v in pairs(BoxHandleAdornment._Drawings) do
+            v.Visible = BoxHandleAdornment.Visible
+            v.Filled = BoxHandleAdornment.Filled
+            v.Color = BoxHandleAdornment.Color3
+            v.Transparency = 1 - BoxHandleAdornment.Transparency
+            v.Thickness = ClampNumber(Distance * 0.01, 0.25, 1)
+
+            if ViewportPoints[i] then
+                v.PointA = ViewportPoints[i][1]
+                v.PointB = ViewportPoints[i][2]
+                v.PointC = ViewportPoints[i][3]
+                v.PointD = ViewportPoints[i][4]
+            end
+        end
+    end))
+
+    return setmetatable(BoxHandleAdornment, MainModule)
+end
+
+function MainModule:IsA(Class)
+    return Class == self.ClassName
+end
+
+function MainModule:Clone()
+    local Clone = MainModule.new()
+    for i,v in pairs(self) do
+        if not i:match("^_") then
+            Clone[i] = v
+        end
+    end
+    return Clone
+end
+
+function MainModule:Destroy()
+    self._Maid:Destroy()
+    for i,v in pairs(self._Drawings) do
+        v:Remove()
+    end
+end
+
+return MainModule
